6ad70e540b609797c52251284bf290c03e5fdfa4
abayomi ogunnusi <abayomiogunnusi@gmail.com>
2023-06-17 20:21:12 +0100

    Updated

--- src/github.com/drsimplegraffiti/crash_course/01_hello/main.go
+++ src/github.com/drsimplegraffiti/crash_course/01_hello/main.go
@@ -5,10 +5,10 @@ import (
 )
 
 func main(){
- fmt.Println("Hello worldvaw😀😀")
+ fmt.Println("Hello world😀😀")
  a := 50
- b := &a
+ b := &a // the & operator gives the address of the variable
 
- c := a + *b
+ c := a + *b // the * operator gives the value stored at an address when you have the address
  fmt.Println(c)
 }
